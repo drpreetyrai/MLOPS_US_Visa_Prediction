7ac376d73f6a0c82f86440b918115b8195982d16
preetyrai1 <drpreetyrai@gmail.com>
2025-04-04 22:19:56 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="drpreetyrai",
+    author_email="drpreetyrai@gmail.com",
+    packages=find_packages()
+)
+
+
+
+
+
